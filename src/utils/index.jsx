c9fec8203a8fb3f0d81caa65ff1367c4bae41a25
Harley Salas <harley@harleysalas.com>
2019-12-08 17:42:02 +0300

    added form util

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,3 +1,4 @@
 export { default as Portal } from "./Portal";
 export { default as useToggle } from "./useToggle";
 export { default as useOnClickOutside } from "./useOnClickOutside";
+export { default as useForm } from "./useform";
