399c514671b6cb3a6903746597c3d07ace318718
Harley Salas <harley@harleysalas.com>
2019-12-09 05:43:14 +0300

    fixed typo

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -2,5 +2,5 @@ export { default as Portal } from "./Portal";
 export { default as ValidateContact } from "./ValidateContact";
 export { default as useToggle } from "./useToggle";
 export { default as useOnClickOutside } from "./useOnClickOutside";
-export { default as useForm } from "./useform";
+export { default as useForm } from "./useForm";
 export { default as useFormValidate } from "./useFormValidate";
