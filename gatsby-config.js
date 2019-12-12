efd3e86a43d41385ace829a93db081ce5258151a
Harley Salas <harley@harleysalas.com>
2019-12-12 16:00:31 +0300

    fixed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
         background_color: `#131316`,
         theme_color: `#131316`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/img/hs-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
